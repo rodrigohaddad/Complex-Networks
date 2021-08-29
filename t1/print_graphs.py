3a0f4fe218b4efe68af7d9b66dbbc0ecaf7ce94a
rodrigohaddad <rgh@poli.ufrj.br>
2021-08-29 15:59:00 -0300

    graphs

--- t1/print_graphs.py
+++ t1/print_graphs.py
@@ -3,63 +3,63 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    # Degree - PMF
-    adj_g_pmf = np.loadtxt('data/adjnouns-degree-pmf.txt', delimiter=',')
+    # Degree - pdf
+    adj_g_pdf = np.loadtxt('data/adjnouns-degree-pdf.txt', delimiter=',')
     adj_g_x = np.loadtxt('data/adjnouns-degree-x.txt', delimiter=',')
 
-    cel_g_pmf = np.loadtxt('data/celegans-degree-pmf.txt', delimiter=',')
+    cel_g_pdf = np.loadtxt('data/celegans-degree-pdf.txt', delimiter=',')
     cel_g_x = np.loadtxt('data/celegans-degree-x.txt', delimiter=',')
 
-    hos_g_pmf = np.loadtxt('data/hospital-degree-pmf.txt', delimiter=',')
+    hos_g_pdf = np.loadtxt('data/hospital-degree-pdf.txt', delimiter=',')
     hos_g_x = np.loadtxt('data/hospital-degree-x.txt', delimiter=',')
 
-    pro_g_pmf = np.loadtxt('data/protein-degree-pmf.txt', delimiter=',')
+    pro_g_pdf = np.loadtxt('data/protein-degree-pdf.txt', delimiter=',')
     pro_g_x = np.loadtxt('data/protein-degree-x.txt', delimiter=',')
 
     plt.figure(figsize=(5, 5))
-    plt.title('Empirical PMF (Degree)')
-    plt.plot(adj_g_x, adj_g_pmf, alpha=0.5, label='Adjnouns')
-    plt.scatter(adj_g_x, adj_g_pmf, alpha=0.5)
-    plt.plot(cel_g_x, cel_g_pmf, alpha=0.5, label='C. Elegans')
-    plt.scatter(cel_g_x, cel_g_pmf, alpha=0.5)
-    plt.plot(hos_g_x, hos_g_pmf, alpha=0.5, label='Hospital')
-    plt.scatter(hos_g_x, hos_g_pmf, alpha=0.5)
-    plt.plot(pro_g_x, pro_g_pmf, alpha=0.5, label='Protein')
-    plt.scatter(pro_g_x, pro_g_pmf, alpha=0.5)
+    plt.title('Empirical PDF (Degree)')
+    plt.plot(adj_g_x, adj_g_pdf, alpha=0.5, label='Adjnouns')
+    plt.scatter(adj_g_x, adj_g_pdf, alpha=0.5)
+    plt.plot(cel_g_x, cel_g_pdf, alpha=0.5, label='C. Elegans')
+    plt.scatter(cel_g_x, cel_g_pdf, alpha=0.5)
+    plt.plot(hos_g_x, hos_g_pdf, alpha=0.5, label='Hospital')
+    plt.scatter(hos_g_x, hos_g_pdf, alpha=0.5)
+    plt.plot(pro_g_x, pro_g_pdf, alpha=0.5, label='Protein')
+    plt.scatter(pro_g_x, pro_g_pdf, alpha=0.5)
     plt.xlabel('Degree')
     plt.ylabel('Probability')
     plt.xlim(0, 60)
     plt.legend()
-    plt.savefig('figs/degree_pmf.jpg')
+    plt.savefig('figs/degree_pdf.jpg')
 
-    # Dista - PMF
-    adj_d_pmf = np.loadtxt('data/adjnouns-dista-pmf.txt', delimiter=',')
+    # Dista - pdf
+    adj_d_pdf = np.loadtxt('data/adjnouns-dista-pdf.txt', delimiter=',')
     adj_d_x = np.loadtxt('data/adjnouns-dista-x.txt', delimiter=',')
 
-    cel_d_pmf = np.loadtxt('data/celegans-dista-pmf.txt', delimiter=',')
+    cel_d_pdf = np.loadtxt('data/celegans-dista-pdf.txt', delimiter=',')
     cel_d_x = np.loadtxt('data/celegans-dista-x.txt', delimiter=',')
 
-    hos_d_pmf = np.loadtxt('data/hospital-dista-pmf.txt', delimiter=',')
+    hos_d_pdf = np.loadtxt('data/hospital-dista-pdf.txt', delimiter=',')
     hos_d_x = np.loadtxt('data/hospital-dista-x.txt', delimiter=',')
 
-    pro_d_pmf = np.loadtxt('data/protein-dista-pmf.txt', delimiter=',')
+    pro_d_pdf = np.loadtxt('data/protein-dista-pdf.txt', delimiter=',')
     pro_d_x = np.loadtxt('data/protein-dista-x.txt', delimiter=',')
 
     plt.figure(figsize=(5, 5))
-    plt.title('Empirical PMF (Distance)')
-    plt.plot(adj_d_x, adj_d_pmf, alpha=0.5, label='Adjnouns')
-    plt.scatter(adj_d_x, adj_d_pmf, alpha=0.5)
-    plt.plot(cel_d_x, cel_d_pmf, alpha=0.5, label='C. Elegans')
-    plt.scatter(cel_d_x, cel_d_pmf, alpha=0.5)
-    plt.plot(hos_d_x, hos_d_pmf, alpha=0.5, label='Hospital')
-    plt.scatter(hos_d_x, hos_d_pmf, alpha=0.5)
-    plt.plot(pro_d_x, pro_d_pmf, alpha=0.5, label='Protein')
-    plt.scatter(pro_d_x, pro_d_pmf, alpha=0.5)
+    plt.title('Empirical PDF (Distance)')
+    plt.plot(adj_d_x, adj_d_pdf, alpha=0.5, label='Adjnouns')
+    plt.scatter(adj_d_x, adj_d_pdf, alpha=0.5)
+    plt.plot(cel_d_x, cel_d_pdf, alpha=0.5, label='C. Elegans')
+    plt.scatter(cel_d_x, cel_d_pdf, alpha=0.5)
+    plt.plot(hos_d_x, hos_d_pdf, alpha=0.5, label='Hospital')
+    plt.scatter(hos_d_x, hos_d_pdf, alpha=0.5)
+    plt.plot(pro_d_x, pro_d_pdf, alpha=0.5, label='Protein')
+    plt.scatter(pro_d_x, pro_d_pdf, alpha=0.5)
     plt.xlabel('Distance')
     plt.ylabel('Probability')
     plt.xlim(0, 10)
     plt.legend()
-    plt.savefig('figs/distance_pmf.jpg')
+    plt.savefig('figs/distance_pdf.jpg')
 
     # Degree - CCDF
     adj_g_ccdf = np.loadtxt('data/adjnouns-degree-ccdf.txt', delimiter=',')
@@ -111,7 +111,7 @@ def main():
     plt.xlabel('Distance')
     plt.ylabel('CCDF')
     plt.legend()
-    plt.savefig('figs/distance-ccdf.jpg')
+    plt.savefig('figs/distance_ccdf.jpg')
 
 
 if __name__ == '__main__':
